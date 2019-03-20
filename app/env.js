e3d53b929b623472d796ed0f07d57a0574b45718
fernandonaghar <fernando.naghar@gmail.com>
2019-03-20 18:08:30 -0300

    endpoint de DEV

--- app/env.js
+++ app/env.js
@@ -1,8 +1,8 @@
 (function (window) {
 
     //DEV Parse
-    Parse.initialize("92f067f59fe76249b67d268b581fee64");
-    Parse.serverURL = 'https://gparsedev.azurewebsites.net/parse';
+    Parse.initialize("b371a60332e07df63ad5e18fb3b59e3f");
+    Parse.serverURL = 'https://gplaycesdev.azurewebsites.net/parse';
     
     facebookAppId = "340536129479663";
 
