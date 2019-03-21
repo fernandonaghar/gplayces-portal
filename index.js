2b1d2abe3b83c9d8cb190bd606726bcad102bf32
fernandonaghar <fernando.naghar@gmail.com>
2019-03-21 09:30:06 -0300

    added index.js for node deployment

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.resolve(__dirname, "app")));
+
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, '/app', 'index.html'));
+});
+
+app.set('port', process.env.PORT || 8080);
+app.listen(app.get('port'), function() {
+    console.log("listening to Port", app.get("port"));
+});
