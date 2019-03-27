8646468080c3da1a0a2a6a79410b1884732959e4
fernandonaghar <fernando.naghar@gmail.com>
2019-03-27 11:21:07 -0300

    fix admin menu responsiveness

--- app/views/internal/sidebar/sidebar.controller.js
+++ app/views/internal/sidebar/sidebar.controller.js
@@ -13,13 +13,12 @@
 
         $rootScope.isAdmin = vm.user.isAdmin;
         vm.profileURL = UserService.GetProfilePhotoURL();
-
+        
         $scope.$watch(function() { return UserService.GetProfilePhotoURL(); },
             function(value) {
                 vm.profileURL = value;
             }
         );
-
     }
 
 })();
